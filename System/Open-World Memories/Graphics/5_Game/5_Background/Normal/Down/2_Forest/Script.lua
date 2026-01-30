5228d8b3a99d6cd7d152f2bce96e855e32cc1246
0auBSQ <58159635+0auBSQ@users.noreply.github.com>
2026-01-31 03:53:08 +0900

    ch7 bar overlap and forest spirits, forest background for OWM

--- System/Open-World Memories/Graphics/5_Game/5_Background/Normal/Down/2_Forest/Script.lua
+++ System/Open-World Memories/Graphics/5_Game/5_Background/Normal/Down/2_Forest/Script.lua
@@ -1,87 +1,110 @@
-local loopWidth = 1920
-local loopHeight = 474
-local downY = 540
+local loopWidth = 1640
 
 local bgClearFade = 0
 
 local bgScrollX = 0
 
-local starsFadeTime = 0
-local starsFade1 = 0
-local starsFade2 = 0
-local starsFade3 = 0
-local starsFade4 = 0
+local clearInAnime_Common = 0
+local clearInAnime_Scroll = 0
+local clearInAnime_Deco = 0
+local clearInAnime_Left = 0
+local clearInAnime_Taiko = 0
+local lightCounter = 0
+
+local taiko_rotate = 0
 
 function clearIn(player)
+    clearInAnime_Common = 0
+    clearInAnime_Scroll = 0
+    clearInAnime_Deco = -0.4
+    clearInAnime_Left = -0.6
+    clearInAnime_Taiko = -0.8
+
+    taiko_rotate = 0.0
 end
 
 function clearOut(player)
 end
 
 function init()
-    func:AddGraph("Day.png");
-    func:AddGraph("Clear_BG.png");
-    func:AddGraph("Clear_FG.png");
-    func:AddGraph("Stars_1.png");
-    func:AddGraph("Stars_2.png");
-    func:AddGraph("Stars_3.png");
-    func:AddGraph("Stars_4.png");
-    func:AddGraph("Clouds.png");
+    func:AddGraph("Sky.png")
+    func:AddGraph("Tatemono.png")
+    func:AddGraph("Tyoutin.png")
+    func:AddGraph("Tyoutin_Light.png")
 
-    if simplemode then
-        starsFade1 = 0.83
-        starsFade2 = 0.75
-        starsFade3 = 0.25
-        starsFade4 = 0.49
-    end
+    func:AddGraph("Down_Scroll.png")
+    func:AddGraph("Down_Clear_Deco.png")
+    func:AddGraph("Down_Clear_Left.png")
+    func:AddGraph("Down_Clear_Taiko.png")
 end
 
 function update()
-    -- Clear fade
     if isClear[0] then
-        bgClearFade = bgClearFade + (2000 * deltaTime);
+        bgClearFade = bgClearFade + (2000 * deltaTime)
     else
-        bgClearFade = bgClearFade - (2000 * deltaTime);
+        bgClearFade = bgClearFade - (2000 * deltaTime)
     end
 
+    lightCounter = lightCounter + (6 * deltaTime)
+
+    clearInAnime_Common = clearInAnime_Common + (1 * deltaTime)
+    clearInAnime_Scroll = clearInAnime_Scroll + (2 * deltaTime)
+    clearInAnime_Deco = clearInAnime_Deco + (2 * deltaTime)
+    clearInAnime_Left = clearInAnime_Left + (2 * deltaTime)
+    clearInAnime_Taiko = clearInAnime_Taiko + (2 * deltaTime)
+
+    if clearInAnime_Common > 1.0 then
+        taiko_rotate = taiko_rotate + (45 * deltaTime)
+    end
+
+    bgScrollX = bgScrollX + (100 * deltaTime)
+    
     if bgClearFade > 255 then
-        bgClearFade = 255;
+        bgClearFade = 255
     end
     if bgClearFade < 0 then
-        bgClearFade = 0;
+        bgClearFade = 0
+    end
+    
+    if bgScrollX > loopWidth then
+        bgScrollX = 0
     end
 
-    if not simplemode then
-        starsFadeTime = starsFadeTime + deltaTime
-
-        starsFade1 = 0.38 * math.sin(((5 * starsFadeTime) / 1.37)) + 0.83
-        starsFade2 = 0.38 * math.cos(((5 * starsFadeTime) / 1.56)) + 0.75
-        starsFade3 = 0.38 * math.sin(((5 * starsFadeTime) / 1.71)) + 0.25
-        starsFade4 = 0.38 * math.cos(((5 * starsFadeTime) / 2.3)) + 0.49
+    if clearInAnime_Scroll > 1 then
+        clearInAnime_Scroll = 1
     end
 
-    -- Cloud scroll
-    if not simplemode then
-        bgScrollX = bgScrollX + (50 * deltaTime);
+    if clearInAnime_Deco > 1 then
+        clearInAnime_Deco = 1
+    end
+    if clearInAnime_Left > 1 then
+        clearInAnime_Left = 1
+    end
+    if clearInAnime_Taiko > 1 then
+        clearInAnime_Taiko = 1
     end
 end
 
 function draw()
-    func:SetOpacity(bgClearFade, "Clear_BG.png");
-    func:SetOpacity(bgClearFade, "Clear_FG.png");
-    func:SetOpacity(bgClearFade, "Clouds.png");
-    func:SetOpacity(bgClearFade * starsFade1, "Stars_1.png");
-    func:SetOpacity(bgClearFade * starsFade2, "Stars_2.png");
-    func:SetOpacity(bgClearFade * starsFade3, "Stars_3.png");
-    func:SetOpacity(bgClearFade * starsFade4, "Stars_4.png");
+    func:SetOpacity(bgClearFade, "Down_Scroll.png")
+    func:SetOpacity(bgClearFade, "Down_Clear_Deco.png")
+    func:SetOpacity(bgClearFade, "Down_Clear_Left.png")
+    func:SetOpacity(bgClearFade, "Down_Clear_Taiko.png")
 
-    func:DrawGraph(0, downY, "Day.png")
+    func:DrawGraph(0, 540, "Sky.png");
+    func:DrawGraph(0, 540, "Tatemono.png");
+    func:DrawGraph(0, 540, "Tyoutin.png");
+    func:SetOpacity(155 - (math.sin(lightCounter * math.pi) * 100), "Tyoutin_Light.png")
+    func:DrawGraph(0, 540, "Tyoutin_Light.png")
+    
+    for i = 0, 3 do
+        func:DrawGraph(0 + (loopWidth * i) - bgScrollX, 540 + ((1.0 - (clearInAnime_Scroll + (math.sin(clearInAnime_Scroll * math.pi) / 2.0))) * 474), "Down_Scroll.png")
+    end
 
-    func:DrawGraph(0, downY, "Clear_BG.png")
-    func:DrawRectGraph(0, downY, bgScrollX * 0.35, 0, loopWidth, loopHeight, "Stars_1.png")
-    func:DrawRectGraph(0, downY, bgScrollX * 0.3, 0, loopWidth, loopHeight, "Stars_2.png")
-    func:DrawRectGraph(0, downY, bgScrollX * 0.25, 0, loopWidth, loopHeight, "Stars_3.png")
-    func:DrawRectGraph(0, downY, bgScrollX * 0.2, 0, loopWidth, loopHeight, "Stars_4.png")
-    func:DrawRectGraph(0, downY, bgScrollX, 0, loopWidth, 474, "Clouds.png")
-    func:DrawGraph(0, downY, "Clear_FG.png")
+    func:DrawGraph(0, 540 + ((1.0 - (clearInAnime_Deco + (math.sin(clearInAnime_Deco * math.pi) / 2.0))) * 474), "Down_Clear_Deco.png")
+    func:DrawGraph(0, 540 + ((1.0 - (clearInAnime_Left + (math.sin(clearInAnime_Left * math.pi) / 2.0))) * 474), "Down_Clear_Left.png")
+    
+    func:SetRotation(taiko_rotate, "Down_Clear_Taiko.png")
+    func:DrawGraph(400 - ((1.0 - (clearInAnime_Taiko + (math.sin(clearInAnime_Taiko * math.pi) / 2.0))) * 700), 540, "Down_Clear_Taiko.png")
+    
 end
